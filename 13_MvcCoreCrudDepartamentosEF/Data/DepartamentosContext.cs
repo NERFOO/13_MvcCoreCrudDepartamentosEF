8afea19d4f8b712dfe4158a4c989993bb7394cc2
NERFOO <james.jaime.1105@gmail.com>
2023-02-16 14:22:24 +0100

    Add project files.

---
+++ 13_MvcCoreCrudDepartamentosEF/Data/DepartamentosContext.cs
@@ -0,0 +1,12 @@
+﻿using _13_MvcCoreCrudDepartamentosEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _13_MvcCoreCrudDepartamentosEF.Data
+{
+    public class DepartamentosContext : DbContext
+    {
+        public DepartamentosContext(DbContextOptions<DepartamentosContext> options) : base(options) { }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
---
+++ 13_MvcCoreCrudDepartamentosEF/Repositories/RepositoryDepartamento.cs
@@ -0,0 +1,30 @@
+﻿using _13_MvcCoreCrudDepartamentosEF.Data;
+using _13_MvcCoreCrudDepartamentosEF.Models;
+
+namespace _13_MvcCoreCrudDepartamentosEF.Repositories
+{
+    public class RepositoryDepartamento
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamento(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Departamento> GetDepartamentos()
+        {
+            var consulta = from datos in this.context.Departamentos
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public Departamento FindDepartamento(int idDepartamento)
+        {
+            var consulta = from datos in this.context.Departamentos
+                           where datos.IdDepartamento == idDepartamento
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+    }
+}
